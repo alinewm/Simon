9203c2e085e961e9a5ea5c773ad25cbb4875b37c
alinewm <alinewada@gmail.com>
2016-10-13 13:31:31 -0400

    Make buttons play sounds

--- GoodSimon/index.js
+++ GoodSimon/index.js
@@ -1,7 +1,32 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function () {
-    //SquareWidget($('#squares'), Square());
-    //SquareWidget($('#squares'), Square());
     BoardWidget();
+    //$('.button').click(function() {
+      //var type = $(this).id;
+      //var sound = $('#audio')[0];
+      //sound.play();
+      //$(this).css('stroke', 'black');
+    //});
+    $('#blue').click(function() {
+      var sound = $('#blueAudio')[0];
+      sound.play();
+      $(this).css('stroke', 'grey');
+    });
+    $('#green').click(function() {
+      var sound = $('#greenAudio')[0];
+      sound.play();
+      $(this).css('stroke', 'black');
+    });
+    $('#red').click(function() {
+      var sound = $('#redAudio')[0];
+      sound.play();
+      $(this).css('stroke', 'brown');
+    });
+    $('#yellow').click(function() {
+      var sound = $('#yellowAudio')[0];
+      sound.play();
+      $(this).css('stroke', 'orange');
+    });
+
   });
 })();
